696419ce6245cbb66d127fdde0821573ce099ccf
Santeri Hänninen <whirloo@gmail.com>
2016-01-01 19:25:15 +0200

    removed shaders from html and reorganized structure

---
+++ shader/fs.glsl
@@ -0,0 +1,45 @@
+  precision mediump float;
+
+  // control
+  // uniform bool uUseLighting;
+  // uniform bool uUseDiffuseTexture;
+  // uniform bool uUseNormalMap;
+
+  // interpolated inputs from vertex shader
+  varying vec4 vPosition;
+  varying vec3 vTransformedNormal;
+  // varying vec2 vTextureCoord;
+  // varying vec4 vColor;
+
+  // material parameters
+  uniform vec4 uDiffuseColor;
+  uniform float uGlossiness;
+
+  // lighting information IN WHAT SPACE???
+  // uniform vec3 uAmbientLighting;
+  // uniform int uNumberOfDirectionalLights;
+  // uniform int uNumberOfPointLights;
+  uniform vec3 uDirectionalLightIntensity;
+  uniform vec3 uDirectionalLightDirection;
+  uniform vec3 uDirectionalLightSpecularColor;
+  // uniform vec3 uPointLightLocation;
+  // uniform vec3 uPointLightDiffuse;
+  // uniform vec3 uPointLightSpecular;
+  // uniform vec3 uMaterialAmbient;
+
+  // uniform sampler2D uSampler;
+
+  void main() {
+    // normal vector
+    vec3 n = normalize(vTransformedNormal);
+    // to-viewer vector
+    vec3 v = vec3(0.0, 0.0, 0.0) - vPosition.rgb;
+    // half-angle vector
+    vec3 h = normalize(uDirectionalLightDirection + v);
+
+    vec3 diffuse = uDiffuseColor.rgb * max(vec3(0.0, 0.0, 0.0), dot(n, normalize(uDirectionalLightDirection)));
+    // vec3 specular = dot(uSpecularColor, pow(max(0.0, dot(h, n)), uGlossiness));
+    vec3 specular = uDirectionalLightSpecularColor * pow(max(dot(h, n), 0.0), uGlossiness);
+
+    gl_FragColor = vec4(uDirectionalLightIntensity * (diffuse + specular), 1);
+  }
---
+++ shader/vs.glsl
@@ -0,0 +1,23 @@
+attribute vec3 aVertexPosition;
+attribute vec3 aVertexNormal;
+// attribute vec2 aTextureCoord;
+// attribute vec4 aVertexColor;
+
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+uniform mat3 uNormalMatrix;
+
+varying vec4 vPosition;
+varying vec3 vTransformedNormal;
+// varying vec2 vTextureCoord
+// varying vec4 vColor;
+
+void main() {
+  vPosition = uModelViewMatrix * vec4(aVertexPosition, 1.0);
+  vTransformedNormal = uNormalMatrix * aVertexNormal;
+  gl_Position = uProjectionMatrix * vPosition;
+
+  // vColor = aVertexColor;
+  // vTransformedNormal = uNMatrix * aVertexNormal;
+  // vTextureCoord = aTextureCoord;
+}
